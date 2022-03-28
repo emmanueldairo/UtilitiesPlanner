bacf78f88943646ebba1a3d213f4ea09197bbc53
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2022-03-28 16:38:15 -0400

    initial check-in of the planner code

---
+++ UtilitiesPlanner/src/test/java/com/UtilitiesPlanner/UtilitiesPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UtilitiesPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitiesPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
